970add54a3cef35439384d04082bbf2ae2651a97
Kyle Lutes <klutes90@gmail.com>
2017-10-02 16:50:33 -0400

    about section left column

---
+++ src/molecules/InnerSection.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+
+const InnerSection = ({ author, quote, contents }) => {
+  return (
+    <Container>
+      <LeftColumn>
+        <Paragraph>
+          <Bold>{quote}</Bold>
+          <Author>- {author}</Author>
+        </Paragraph>
+        {contents.map(content => <Paragraph>{content}</Paragraph>)}
+      </LeftColumn>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: calc(6rem + (8 - 6) * (100vw - 76.8rem)/ (110 - 76.8));
+  @media all and (min-width: 50em) {
+    margin-top: 8rem;
+  }
+`;
+
+const LeftColumn = styled.div`
+  order: 2;
+  width: 45%;
+  @media all and (min-width: 50em) {
+    order: 1;
+  }
+`;
+
+const Paragraph = styled.p`
+  margin-top: 0;
+  line-height: 1.75;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Bold = styled.span`font-weight: 700;`;
+
+const Author = styled.span`
+  font-size: 1.6rem;
+  text-transform: uppercase;
+`;
+
+export default InnerSection;
